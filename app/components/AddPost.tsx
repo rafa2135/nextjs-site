39005674fb92a91efa702f75333115a86b6b1b80
Rafael Villalobos <rafaelvillalobos_20@hotmail.com>
2023-03-11 01:13:47 -0400

    added Nav login Logged AddPost

---
+++ app/components/AddPost.tsx
@@ -0,0 +1,34 @@
+"use client";
+import { useState } from "react";
+
+export default function CreatePost() {
+  const [title, settitle] = useState("");
+  const [isDisabled, setIsDisabled] = useState(false);
+  return (
+    <form className="bg-white my-6 p-4 rounded-md">
+      <div className="flex flex-col my-4">
+        <textarea
+          onChange={(e) => settitle(e.target.value)}
+          name="title"
+          value={title}
+          placeholder="What's on your mind?"
+          className="p-4 text-lg rounded-md my-2 bg-gray-200"
+        ></textarea>
+      </div>
+      <div className="flex items-center justify-between gap-2">
+        <p
+          className={`font-bold text-sm ${
+            title.length > 300 ? "text-red-700" : " text-gray-700"
+          }`}
+        >{`${title.length}/300`}</p>
+        <button
+          disabled={isDisabled}
+          type="submit"
+          className="text-sm bg-teal-600 text-white py-2 px-6 rounded-xl disabled:opacity-25"
+        >
+          Create a post
+        </button>
+      </div>
+    </form>
+  );
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,10 @@
+"use client";
+import AddPost from "./components/AddPost";
 export default function Home() {
   return (
     <main>
-      <h1> Hello next</h1>
+      <h1> Hello next !</h1>
+      <AddPost />
     </main>
   );
 }
